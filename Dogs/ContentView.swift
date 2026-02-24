0328f1fd2a7e364c20182bfe160d95803666f0ea
mollynorman <norman.j.molly@gmail.com>
2026-02-24 10:39:52 -0600

    feat: Added Image from Dog.CEO.

--- Dogs/ContentView.swift
+++ Dogs/ContentView.swift
@@ -10,11 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            AsyncImage(url: URL(string: "https://images.dog.ceo/breeds/hound-english/n02089973_2331.jpg")) { img in
+                if let error = img.error{
+                    Text("We have an error!")
+                    Text("\(error.localizedDescription)")
+                }
+                if let image = img.image {
+                    image
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                }
+            }
+            Text("Maverick")
+                .bold()
+                .font(.largeTitle)
+            
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .padding()
     }
 }
