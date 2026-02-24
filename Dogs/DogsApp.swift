ea5c773acab964203c9dfb8914a422e0db5ec9cb
mollynorman <norman.j.molly@gmail.com>
2026-02-24 10:19:25 -0600

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Molly Norman on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
